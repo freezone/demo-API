ba61a8b3663beb21ab41fba7ccc01de4fdf222d4
freezone <frison.enrico@gmail.com>
2024-03-03 17:18:56 +0000

    Authentication

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bae2fec3abe0e19a1d7b131e1cc62db5bd9a7500")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5858f3f5151bc1ac5022f8d0a9789f428f7a18e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
